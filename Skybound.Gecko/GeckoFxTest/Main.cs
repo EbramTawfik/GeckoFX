63187147dbb7975cf2d7fa6fad69f0eba1aa5d6b
Tom Hindle <tom_hindle@sil.org>
2010-10-14 17:22:29 -0600

    Expanding test case.

--- Skybound.Gecko/GeckoFxTest/Main.cs
+++ Skybound.Gecko/GeckoFxTest/Main.cs
@@ -1,5 +1,9 @@
 using System;
+using System.Windows.Forms;
+using Skybound.Gecko;
 
+// You will need to run this with a new version of mono (2.8 or newer)
+// mono 2.6.3 can not handle this.
 namespace GeckoFxTest
 {
 	class MainClass
@@ -7,7 +11,19 @@ namespace GeckoFxTest
 		public static void Main(string[] args)
 		{
 			// TODO: make better way of finding libxul.so etc.
-			Skybound.Gecko.Xpcom.Initialize("/usr/lib/xulrunner/");
+			Xpcom.Initialize("/usr/lib/xulrunner/");
+			
+			Application.Run(new MyForm());			
+		}
+	}
+	
+	class MyForm : Form
+	{
+		public MyForm()
+		{
+			var browser = new GeckoWebBrowser();			
+			browser.Dock = DockStyle.Fill;
+			this.Controls.Add(browser);
 		}
 	}
 }
