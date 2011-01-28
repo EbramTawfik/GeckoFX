4323894806364104e8025772f68abcd275c78ab4
Tom Hindle <tom_hindle@sil.org>
2011-01-27 21:00:40 -0700

    Move classes into there own files.

---
+++ Skybound.Gecko/GtkOnceOnly.cs
@@ -0,0 +1,21 @@
+namespace GtkDotNet
+{
+	public static class GtkOnceOnly
+	{
+		public static void Init()
+		{
+			lock(m_initedOnce)
+			{
+				if (((bool)m_initedOnce) == false)
+				{
+					Gtk.Application.Init();
+					m_initedOnce = true;
+				}
+			}
+		}
+		/// <summary>
+		/// Stores if gtk has been initizlized.
+		/// </summary>
+		internal static object m_initedOnce = false;	
+	}
+}
