2beb8de2e8714483b1a36cf86cf32ad1d870116c
Neil Mayhew <neil_mayhew@sil.org>
2011-03-16 10:36:10 -0600

    Fix crashing bug in XULRunnerLocator.GetXULRunnerLocation()
    The crash happens only when there is more than one xulrunner dir.

--- Skybound.Gecko/XULRunnerLocator.cs
+++ Skybound.Gecko/XULRunnerLocator.cs
@@ -5,9 +5,9 @@ using System.Linq;
 namespace Skybound.Gecko
 {
 	/// <summary>
-	/// Search /usr/lib directory for latest xulrunner.
-	/// Currenly looks for xulrunner-1.9.2 variants in /usr/lib.
-	/// returns null if xulrunner-1. directory isn't found.
+	/// Search /usr/lib directory for the latest xulrunner.
+	/// Currently looks for xulrunner-1.9.2 variants in /usr/lib.
+	/// Returns null if no xulrunner-1.9.2 directory is found.
 	/// xulrunner doesn't come with a .pc file so I can't use pkg-config to find location of xulrunner.
 	/// </summary>
 	public static class XULRunnerLocator
@@ -16,21 +16,17 @@ namespace Skybound.Gecko
 		{
 			if (!Xpcom.IsLinux)
 				throw new ApplicationException("XULRunnerLocator only works on Linux.");
-			
+
 			if (!Directory.Exists("/usr/lib"))
 				throw new ApplicationException("/usr/lib doesn't exist");
-			
+
 			DirectoryInfo d = new DirectoryInfo("/usr/lib");
 			DirectoryInfo[] dirs = d.GetDirectories("xulrunner-1.9.2.*", SearchOption.TopDirectoryOnly);
-			
+
 			if (dirs.Length == 0)
 				return null;
-			
-			if (dirs.Length == 1)
-				return dirs[0].FullName;
-						
-			return dirs.OrderBy(x => x).First().FullName;						
+
+			return dirs.OrderBy(x => x.Name).Last().FullName;
 		}
 	}
 }
-
