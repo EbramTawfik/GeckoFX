a9e6778303a7b60c4a62509b1c14f3d9e9e77f7d
hindlet <hindlet@localhost>
2011-10-14 13:22:32 +0100

    Add GetData extension method - based upon patch by Salaros.

---
+++ Skybound.Gecko/GeckoElementExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Skybound.Gecko
+{
+	static class GeckoElementExtensions
+	{
+		/// <summary>
+		/// Gets the value of the data-xxx attributes
+		/// </summary>
+		/// <param name="dataAttributeName">Microdata label in data-label attribute</param>
+		/// <returns></returns>
+		public static string GetData(this GeckoElement node, string dataAttributeName)
+		{
+			if (String.IsNullOrEmpty(dataAttributeName) || dataAttributeName.Trim().Length == 0)
+				throw new ArgumentException("dataAttributeName");
+			
+			if (node.Attributes["data-" + dataAttributeName] == null)
+				return string.Empty;
+			
+			return node.Attributes["data-" + dataAttributeName].TextContent;			
+		} 
+	}
+}
