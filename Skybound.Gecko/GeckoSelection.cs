c09ff268b0887ba954778002e1e7297f4f64ee01
hindlet <hindlet@localhost>
2011-12-20 09:45:41 -0700

    Add ScrollIntoView to GeckoSelection.

--- Skybound.Gecko/GeckoSelection.cs
+++ Skybound.Gecko/GeckoSelection.cs
@@ -231,7 +231,44 @@ namespace Gecko
 		{
 			((nsISelectionPrivate)Selection).EndBatchChanges();
 		}
-		
+
+		/// <summary>
+		/// Scrolls a region of the selection, so that it is visible in
+		/// the scrolled view.
+		///
+		/// @param aRegion - the region inside the selection to scroll into view
+		/// (see selection region constants defined in
+		/// nsISelectionController).
+		/// @param aIsSynchronous - when true, scrolls the selection into view
+		/// before returning. If false, posts a request which
+		/// is processed at some point after the method returns.
+		/// @param aVPercent - how to align the frame vertically. A value of 0
+		/// means the frame's upper edge is aligned with the top edge
+		/// of the visible area. A value of 100 means the frame's
+		/// bottom edge is aligned with the bottom edge of
+		/// the visible area. For values in between, the point
+		/// "aVPercent" down the frame is placed at the point
+		/// "aVPercent" down the visible area. A value of 50 centers
+		/// the frame vertically. A value of -1 means move
+		/// the frame the minimum amount necessary in order for
+		/// the entire frame to be visible vertically (if possible).
+		/// @param aHPercent - how to align the frame horizontally. A value of 0
+		/// means the frame's left edge is aligned with the left
+		/// edge of the visible area. A value of 100 means the
+		/// frame's right edge is aligned with the right edge of
+		/// the visible area. For values in between, the point
+		/// "aHPercent" across the frame is placed at the point
+		/// "aHPercent" across the visible area. A value of 50
+		/// centers the frame horizontally . A value of -1 means
+		/// move the frame the minimum amount necessary in order
+		/// for the entire frame to be visible horizontally
+		/// (if possible).
+		/// </summary>
+		public void ScrollIntoView(short aRegion, bool aIsSynchronous, short aVPercent, short aHPercent)
+		{
+			((nsISelectionPrivate)Selection).ScrollIntoView(aRegion, aIsSynchronous, aVPercent, aHPercent);
+		}
+
 		/// <summary>
 		/// Returns the whole selection as a plain text string.
 		/// </summary>
