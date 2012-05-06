e1381631c1c9a54172c9c4eaa915cf4dea54522b
shcherba <shcherba@RU01LW70005.ee.intra-global.net>
2012-05-07 02:03:52 +0400

    + WindowWatcher service wrapper

--- Skybound.Gecko/GeckoWebBrowser.cs
+++ Skybound.Gecko/GeckoWebBrowser.cs
@@ -234,13 +234,9 @@ namespace Gecko
 		{
 			static WindowCreator()
 			{
-				// give an nsIWindowCreator to the WindowWatcher service
-				nsIWindowWatcher watcher = Xpcom.GetService<nsIWindowWatcher>("@mozilla.org/embedcomp/window-watcher;1");
-				if (watcher != null)
-				{
-					//disabled for now because it's not loading the proper URL automatically
-					watcher.SetWindowCreator(new WindowCreator());
-				}
+				// Register it to window watcher service
+
+				Services.WindowWatcher.WindowCreator = new WindowCreator();
 			}
 			
 			public static void Register()
