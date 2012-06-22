db329ddaba6ef4ac21a365e9037e52cf8bf66560
daniel <daniel@localhost>
2012-06-22 16:17:54 +0700

    compile jemalloc with -fPIC

--- GeckoFxTest/GeckoFxTest.sh
+++ GeckoFxTest/GeckoFxTest.sh
@@ -3,7 +3,7 @@
 if [ ! -f ./jemallocfix.so ]
 then
 # jemallocfix for firefox 10+
-    echo -e "#include<stdlib.h>\nsize_t je_malloc_usable_size_in_advance(size_t n){ return n; }\nvoid * moz_xrealloc(void *ptr, size_t size) { return realloc(ptr, size); }" | gcc -xc --shared - -o jemallocfix.so
+    echo -e "#include<stdlib.h>\nsize_t je_malloc_usable_size_in_advance(size_t n){ return n; }\nvoid * moz_xrealloc(void *ptr, size_t size) { return realloc(ptr, size); }" | gcc -xc --shared -fPIC - -o jemallocfix.so
     strip jemallocfix.so
 fi
 
@@ -13,4 +13,4 @@ export MONO_PATH=/usr/lib/cli/gdk-sharp-2.0
 export LD_PRELOAD=./jemallocfix.so
 
 # use need to use a mono with this patch in it https://bugzilla.novell.com/show_bug.cgi?id=672879
-mono -debug GeckoFxTest.exe
+mono --debug GeckoFxTest.exe
