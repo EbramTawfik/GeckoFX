7dfa78a81c65fead8e991b0dcafcadd75b6e50bd
vmas <vmas@localhost>
2013-04-23 20:00:58 +0400

    Added Style and ComputedStyle to XulElement

--- Geckofx-Core/DOM/Xul/XulElement.cs
+++ Geckofx-Core/DOM/Xul/XulElement.cs
@@ -19,5 +19,36 @@ namespace Gecko.DOM.Xul
 		{
 			return xulElement == null ? null : new XulElement( xulElement );
 		}
+
+		/// <summary>
+		/// Gets the inline style of the XulElement. 
+		/// </summary>
+		public GeckoStyle Style
+		{
+			get
+			{
+				return GeckoStyle.Create(Xpcom.QueryInterface<nsIDOMElementCSSInlineStyle>(DomObject).GetStyleAttribute());
+			}
+		}
+
+		/// <summary>
+		/// Gets style of the XulElement. 
+		/// </summary>
+		public GeckoStyle ComputedStyle
+		{
+			get
+			{
+				nsIDOMCSSStyleDeclaration style;
+				using (var element = new InstanceWrapper<nsIDOMElement>(Xpcom.QueryInterface<nsIDOMElement>(this.DomObject)))
+				{
+					using (var nullString = new nsAString())
+					{
+						nullString.SetData(null);
+						style = this.OwnerDocument.DefaultView.DomWindow.GetComputedStyle(element.Instance, nullString);
+					}
+				}
+				return GeckoStyle.Create(style);
+			}
+		}
 	}
 }
