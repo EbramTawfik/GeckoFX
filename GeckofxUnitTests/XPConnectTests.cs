60c670e830c8e0e97d7b37413f7359ad04258725
hindlemail <hindlemail@localhost>
2014-01-02 10:00:12 -0700

    Add unittests for nsIXpConnectWrappedNative methods FindInterfaceWithName and FindInterfaceWithMember. Also add some test code showing interface spec problems with nsIClassInfo.

--- GeckofxUnitTests/XPConnectTests.cs
+++ GeckofxUnitTests/XPConnectTests.cs
@@ -13,6 +13,8 @@ namespace GeckofxUnitTests
 	{
 		nsIXPConnect m_instance;
 
+		private GeckoWebBrowser _browser;
+
 		[SetUp]
 		public void BeforeEachTestSetup()
 		{
@@ -23,6 +25,10 @@ namespace GeckofxUnitTests
 			Assert.IsNotNull(ptr);
 			m_instance = (nsIXPConnect)Xpcom.GetObjectForIUnknown(ptr);
 			Assert.IsNotNull(m_instance);
+
+			_browser = new GeckoWebBrowser();
+			var unused = _browser.Handle;
+			Assert.IsNotNull(_browser);
 		}
 
 		[TearDown]
@@ -36,6 +42,124 @@ namespace GeckofxUnitTests
 		{
 			Assert.IsNull(m_instance.GetCurrentJSStackAttribute());
 		}
+
+		[Test]
+		public void FindInterfaceWithMember_OnADocumentElementLookingForNodeNameMethod_ReturnsInterfaceThatContainsMethodNode()
+		{
+			_browser.TestLoadHtml("hello world");
+			using (AutoJSContext context = new AutoJSContext(_browser.Window.JSContext))
+			{
+				Guid guid = typeof(nsISupports).GUID;
+				IntPtr globalObject = SpiderMonkey.JS_GetGlobalForScopeChain(context.ContextPointer);
+				var wrapper = Xpcom.XPConnect.Instance.WrapNative(context.ContextPointer, globalObject, (nsISupports)_browser.Document.DomObject, ref guid);
+				var a = m_instance.GetWrappedNativeOfJSObject(context.ContextPointer, wrapper.GetJSObjectAttribute());
+				var jsVal = SpiderMonkeyTests.CreateStringJsVal("nodeName");
+				var i = a.FindInterfaceWithMember(jsVal.AsPtr);
+				Assert.NotNull(i);
+				Assert.IsTrue(i.IsScriptable());
+				Assert.AreEqual("nsIDOMHTMLDocument", i.GetNameShared());
+			}
+		}
+
+		[Test]
+		public void FindInterfaceWithName_OnADocumentElementLookingDOMHTMLDocumentInterface_ReturnsExpectedInterface()
+		{
+			_browser.TestLoadHtml("hello world");
+			using (AutoJSContext context = new AutoJSContext(_browser.Window.JSContext))
+			{
+				Guid guid = typeof(nsISupports).GUID;
+				IntPtr globalObject = SpiderMonkey.JS_GetGlobalForScopeChain(context.ContextPointer);
+				var wrapper = Xpcom.XPConnect.Instance.WrapNative(context.ContextPointer, globalObject, (nsISupports)_browser.Document.DomObject, ref guid);
+				var a = m_instance.GetWrappedNativeOfJSObject(context.ContextPointer, wrapper.GetJSObjectAttribute());
+				var jsVal = SpiderMonkeyTests.CreateStringJsVal("nsIDOMHTMLDocument");
+				var i = a.FindInterfaceWithName(jsVal.AsPtr);
+				Assert.NotNull(i);
+				Assert.IsTrue(i.IsScriptable());
+				Assert.AreEqual("nsIDOMHTMLDocument", i.GetNameShared());
+			}
+		}
+
+		#region test showing bug https://bitbucket.org/geckofx/geckofx-22.0/issue/35/invalid-interface
+
+		[Ignore("https://bitbucket.org/geckofx/geckofx-22.0/issue/35/invalid-interface")]
+		[Test]
+		public void GetWrappedNativePrototype()
+		{
+			_browser.TestLoadHtml("<span id='a'>hello world</span>");
+			var element = _browser.Document.GetHtmlElementById("a");
+
+			using (AutoJSContext context = new AutoJSContext(_browser.Window.JSContext))
+			{				
+				IntPtr globalObject = SpiderMonkey.JS_GetGlobalForScopeChain(context.ContextPointer);
+				var jsobjectholder = m_instance.GetWrappedNativePrototype(context.ContextPointer, globalObject, new MyClassInfo());
+				
+				string result;
+				Console.WriteLine(context.EvaluateScript("this", (nsISupports)jsobjectholder, out result));
+				Console.WriteLine(result);				
+			}			
+		}
+
+		public class MyClassInfo : nsIClassInfo
+		{
+			public void GetInterfaces(ref uint count, ref Guid[] array)
+			{
+				count = 1;
+				array = new Guid[1];
+				array[0] = typeof(nsIDOMDocument).GUID;
+			}
+#if false
+			public void GetInterfaces(ref uint count, ref IntPtr array)
+			{
+				count = 1;				
+				array = Xpcom.Alloc(new IntPtr(IntPtr.Size * count));
+				Guid guid = typeof (nsIDOMDocument).GUID;
+				var bytearray = guid.ToByteArray();
+				IntPtr guidPtr = Xpcom.Alloc(new IntPtr(16));
+				for (int i = 0; i < 16; i++)
+				{
+					Marshal.WriteByte(guidPtr, i, bytearray[i]);
+				}				
+				Marshal.WriteIntPtr(array, guidPtr);		
+			}
+#endif
+
+			public nsISupports GetHelperForLanguage(uint language)
+			{
+				return null;
+			}
+
+			public string GetContractIDAttribute()
+			{
+				throw new NotImplementedException();
+			}
+
+			public string GetClassDescriptionAttribute()
+			{
+				throw new NotImplementedException();
+			}
+
+			public IntPtr GetClassIDAttribute()
+			{
+				throw new NotImplementedException();
+			}
+
+			public uint GetImplementationLanguageAttribute()
+			{
+				throw new NotImplementedException();
+			}
+
+			public uint GetFlagsAttribute()
+			{
+				return 1 << 3;
+			}
+
+			public IntPtr GetClassIDNoAllocAttribute()
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		#endregion
 
 		[Ignore("randomly crashes")]
 		[Test]
