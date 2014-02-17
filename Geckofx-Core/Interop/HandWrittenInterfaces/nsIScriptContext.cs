92826fb1c22f099b7750cf4e4d2d89419c8ea4a2
hindlemail <hindlemail@localhost>
2014-02-17 12:29:46 -0700

    Add nsIScriptContext interface.

---
+++ Geckofx-Core/Interop/HandWrittenInterfaces/nsIScriptContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Gecko
+{
+	[ComImport]
+	[InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+	[Guid("513c2c1a-f4f1-44da-8e38-f40c309a5def")]
+	public interface nsIScriptContext
+	{
+		// Methods on this interface aren't directly callable via (XP)COM, due to
+		// them having the wrong calling convention. It may be possible to invoke the
+		// methods by via the vtable.
+	}
+}
