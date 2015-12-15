828aca663d73b47fce8a7544c21007ecaffa879b
hindlemail <hindlemail@localhost>
2015-12-15 12:23:15 -0700

    Add missing file.

---
+++ Geckofx-Core/BaseTypes/gfxSize.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Gecko
+{
+    // TODO: make this class binary marshalable from struct gfxSize
+	[StructLayout(LayoutKind.Sequential)]
+	public class gfxSize
+	{
+		public double Width;
+		public double Height;
+	}
+}
