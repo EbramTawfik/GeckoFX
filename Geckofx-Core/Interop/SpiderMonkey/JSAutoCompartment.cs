e82fab2b3643b5a11fefcf690eca4405e2465101
sbetzin <lovegirl1@localhost>
2016-02-03 00:49:18 +0100

    refactored to avoid the code smell: DRY at the creation of the _oldCompartmentScope

--- Geckofx-Core/Interop/SpiderMonkey/JSAutoCompartment.cs
+++ Geckofx-Core/Interop/SpiderMonkey/JSAutoCompartment.cs
@@ -6,53 +6,61 @@ using System.Text;
 
 namespace Gecko
 {
-	public sealed class JSAutoCompartment : IDisposable
-	{
-		private IntPtr _oldCompartment;
-		private IntPtr _cx;
-		private IntPtr _obj;
-		private bool _isDisposed;
+    public sealed class JSAutoCompartment : IDisposable
+    {
+        private IntPtr _oldCompartment;
+        private readonly IntPtr _cx;
+        private readonly IntPtr _obj;
+        private bool _isDisposed;
 
-		public JSAutoCompartment(AutoJSContext context, nsISupports comObject)
-		{
-			if (context == null)
-				throw new ArgumentNullException("context");
-			if (context.ContextPointer == IntPtr.Zero)
-				throw new ArgumentException("context has Null ContextPointer");
-			if (context == null)
-				throw new ArgumentNullException("comObject");
+        public JSAutoCompartment(AutoJSContext context, nsISupports comObject)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+            if (context.ContextPointer == IntPtr.Zero)
+                throw new ArgumentException("context has Null ContextPointer");
+            if (context == null)
+                throw new ArgumentNullException("comObject");
 
-			_obj = context.ConvertCOMObjectToJSObject(comObject);
-			_cx = context.ContextPointer;
-			_oldCompartment = SpiderMonkey.JS_EnterCompartment(_cx, _obj);			
-		}
+            _obj = context.ConvertCOMObjectToJSObject(comObject);
+            _cx = context.ContextPointer;
+
+            EnterOldCompartment();
+        }
 
 
-		public JSAutoCompartment(IntPtr context, IntPtr obj)
-		{
-			if (context == IntPtr.Zero)
-				throw new ArgumentNullException("context");
-			if (context == IntPtr.Zero)
-				throw new ArgumentNullException("obj");
+        public JSAutoCompartment(IntPtr context, IntPtr obj)
+        {
+            if (context == IntPtr.Zero)
+                throw new ArgumentNullException("context");
+            if (context == IntPtr.Zero)
+                throw new ArgumentNullException("obj");
 
-			_obj = obj;
-			_oldCompartment = SpiderMonkey.JS_EnterCompartment(context, _obj);
-			_cx = context;
-		}
+            _obj = obj;
+            _cx = context;
 
-		public IntPtr ScopeObject { get { return _obj; } }
+            EnterOldCompartment();
+        }
 
-		public void Dispose()
-		{
-			if (!_isDisposed)
-			{
-				_isDisposed = true;
-				if (_cx != IntPtr.Zero)
-				{
-					SpiderMonkey.JS_LeaveCompartment(_cx, _oldCompartment);
-				}
-				GC.SuppressFinalize(this);
-			}
-		}
-	}
+        private void EnterOldCompartment()
+        {
+            _oldCompartment = SpiderMonkey.JS_EnterCompartment(_cx, _obj);
+        }
+
+        public IntPtr ScopeObject { get { return _obj; } }
+
+        public void Dispose()
+        {
+            if (_isDisposed)
+            {
+                return;
+            }
+            _isDisposed = true;
+            if (_cx != IntPtr.Zero)
+            {
+                SpiderMonkey.JS_LeaveCompartment(_cx, _oldCompartment);
+            }
+            GC.SuppressFinalize(this);
+        }
+    }
 }
