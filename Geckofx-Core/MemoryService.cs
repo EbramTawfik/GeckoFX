4fe335423bbb5565be3070e94b6af05a43a85bbf
sbetzin <lovegirl1@localhost>
2016-02-03 11:34:43 +0100

    Added Memory Service to allow mimimizing Heap from applications

---
+++ Geckofx-Core/MemoryService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Gecko
+{
+    public static class MemoryService
+    {
+        public static void MinimizeHeap(bool immeadiate)
+        {
+            var memoryService = Xpcom.GetService<nsIMemory>("@mozilla.org/xpcom/memory-service;1");
+            try
+            {
+                memoryService.HeapMinimize(immeadiate);
+            }
+            catch (Exception)
+            {
+                //Ignore
+            }
+            finally
+            {
+                Marshal.ReleaseComObject(memoryService);
+            }
+        }
+    }
+}
