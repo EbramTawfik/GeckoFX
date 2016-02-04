9a58749dfcb4aee3f84bd4757ce1a06fcd91b62a
sbetzin <lovegirl1@localhost>
2016-02-04 16:18:39 +0100

    Extension methods that are much more conveinient to execute JQueries...

---
+++ Geckofx-Core/JQuery/GeckoWebBrowserExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Gecko.JQuery
+{
+    public static class GeckoWebBrowserExtensions
+    {
+        public static void  ExecuteJQuery(this IGeckoWebBrowser browser, string jQuery)
+        {
+            var jquery = new JQueryExecutor(browser.Window);
+
+            jquery.ExecuteJQuery(jQuery);
+        }
+
+        public static GeckoElement GetElementByJQuery(this IGeckoWebBrowser browser, string jQuery)
+        {
+            var jquery = new JQueryExecutor(browser.Window);
+
+            return jquery.GetElementByJQuery(jQuery);
+        }
+
+        public static IEnumerable<GeckoElement> GetElementsByJQuery(this IGeckoWebBrowser browser, string jQuery)
+        {
+            var jquery = new JQueryExecutor(browser.Window);
+
+            return jquery.GetElementsByJQuery(jQuery);
+        }
+    }
+}
