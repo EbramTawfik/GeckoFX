860919d9b302f757c5e69b985867a540fdc67f6f
hindlemail <hindlemail@localhost>
2016-02-11 10:28:28 -0700

    Some fixes towards making geckofx 45 work on Linux.

--- Geckofx-Core/AutoJSContext.cs
+++ Geckofx-Core/AutoJSContext.cs
@@ -98,7 +98,7 @@ namespace Gecko
             using (var xpc = new ComPtr<nsIGlobalObject>(go, false))
             {
                 int slot = xpc.GetSlotOfComMethod(new Func<IntPtr>(go.GetGlobalJSObject));
-                var getGlobalJSObject = xpc.GetComMethod<GetGlobalJSObject>(Xpcom.IsLinux ? -1 : slot);
+                var getGlobalJSObject = xpc.GetComMethod<GetGlobalJSObject>(Xpcom.IsLinux ? 3 : slot);
                 _globalJSObject = getGlobalJSObject(go);
                 if (_globalJSObject == IntPtr.Zero)
                     throw new ArgumentException(
