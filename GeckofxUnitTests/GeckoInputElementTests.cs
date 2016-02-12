4b8b916bdf99a9bbd30d4f0787535424e38795bb
hindlemail <hindlemail@localhost>
2016-02-11 19:32:46 -0700

    Code cleanup.

--- GeckofxUnitTests/GeckoInputElementTests.cs
+++ GeckofxUnitTests/GeckoInputElementTests.cs
@@ -13,51 +13,52 @@ using Gecko.DOM;
 namespace GeckofxUnitTests
 {
     // Using SetCulture to catch bugs where ToLower is used instead of ToLowerInvariant
-	[TestFixture]
+    [TestFixture]
     [SetCulture("tr-TR")]
-	internal class GeckoInputElementTests
-	{
-		private GeckoWebBrowser browser;
+    internal class GeckoInputElementTests
+    {
+        private GeckoWebBrowser browser;
 
-		[SetUp]
-		public void BeforeEachTestSetup()
-		{
-			Xpcom.Initialize(XpComTests.XulRunnerLocation);
-			browser = new GeckoWebBrowser();
-			var unused = browser.Handle;
-			Assert.IsNotNull(browser);
-		}
+        [SetUp]
+        public void BeforeEachTestSetup()
+        {
+            Xpcom.Initialize(XpComTests.XulRunnerLocation);
+            browser = new GeckoWebBrowser();
+            var unused = browser.Handle;
+            Assert.IsNotNull(browser);
+        }
 
-		[TearDown]
-		public void AfterEachTestTearDown()
-		{
-			browser.Dispose();
-		}
+        [TearDown]
+        public void AfterEachTestTearDown()
+        {
+            browser.Dispose();
+        }
 
-		[Test]
-		public void SelectionStart_NoSelection_ReturnsZero()
-		{
-			browser.TestLoadHtml(@"<input type=""text"" id=""txtbox"" value=""text""/>");
+        [Test]
+        public void SelectionStart_NoSelection_ReturnsZero()
+        {
+            browser.TestLoadHtml(@"<input type=""text"" id=""txtbox"" value=""text""/>");
 
-			GeckoInputElement element = (GeckoInputElement)browser.Document.GetHtmlElementById("txtbox");
-			Assert.AreEqual(0, element.SelectionStart);
-		}
+            GeckoInputElement element = (GeckoInputElement) browser.Document.GetHtmlElementById("txtbox");
+            Assert.AreEqual(0, element.SelectionStart);
+        }
 
-		[Test]
-		public void SelectionEnd_NoSelection_ReturnsZero()
-		{
-			browser.TestLoadHtml(@"<input type=""text"" id=""txtbox"" value=""text""/>");
+        [Test]
+        public void SelectionEnd_NoSelection_ReturnsZero()
+        {
+            browser.TestLoadHtml(@"<input type=""text"" id=""txtbox"" value=""text""/>");
 
-			GeckoInputElement element = (GeckoInputElement)browser.Document.GetHtmlElementById("txtbox");
-			Assert.AreEqual(0, element.SelectionEnd);
-		}
+            GeckoInputElement element = (GeckoInputElement) browser.Document.GetHtmlElementById("txtbox");
+            Assert.AreEqual(0, element.SelectionEnd);
+        }
 
         [Test]
         public void Click_JavascriptOnClickRuns()
         {
-            browser.TestLoadHtml(@"<input type=""text"" id=""txtbox"" value=""text"" onclick=""this.value = 'clicked';""/>");
+            browser.TestLoadHtml(
+                @"<input type=""text"" id=""txtbox"" value=""text"" onclick=""this.value = 'clicked';""/>");
 
-            var element = (GeckoInputElement)browser.Document.GetHtmlElementById("txtbox");
+            var element = (GeckoInputElement) browser.Document.GetHtmlElementById("txtbox");
             element.Click();
 
             Assert.AreEqual("clicked", element.Value);
@@ -66,11 +67,12 @@ namespace GeckofxUnitTests
         [Test]
         public void Click_AfterMouseUpAndMouseDownEvents_ExpectedJavascriptHandlerRun()
         {
-            browser.TestLoadHtml(@"<input type=""text"" id=""txtbox"" value=""text"" onmousedown=""this.value = 'mousedowned';"" onmouseup=""this.value = 'mouseuped';"" onclick=""this.value = 'clicked';""/>");
+            browser.TestLoadHtml(
+                @"<input type=""text"" id=""txtbox"" value=""text"" onmousedown=""this.value = 'mousedowned';"" onmouseup=""this.value = 'mouseuped';"" onclick=""this.value = 'clicked';""/>");
 
-            GeckoInputElement element = (GeckoInputElement)browser.Document.GetHtmlElementById("txtbox");
+            GeckoInputElement element = (GeckoInputElement) browser.Document.GetHtmlElementById("txtbox");
 
-            
+
             DomEventArgs ev = browser.Document.CreateEvent("MouseEvent");
             var webEvent = new Event(browser.Window.DomWindow, ev.DomEvent as nsISupports);
             webEvent.InitEvent("mousedown", true, true);
@@ -87,5 +89,5 @@ namespace GeckofxUnitTests
 
             Assert.AreEqual("clicked", element.Value);
         }
-	}
+    }
 }
